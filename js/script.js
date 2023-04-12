cce06921043efa2a0eb71d56c8232123917f2b14
Luka <luka.luka.luka93@gmail.com>
2023-04-12 16:10:19 -0700

    Grocery list

---
+++ js/script.js
@@ -0,0 +1,46 @@
+var groceries = [
+  "milk",
+  "coffee",
+  "oranges",
+  "yogourt",
+  "bread",
+  "juice",
+  "lettuce",
+  "flour",
+  "apples",
+  "milk",
+  "cat food",
+  "juice",
+  "ice cream",
+  "potatoes",
+  "bananas",
+  "coffee",
+  "olive oil",
+  "mustard"
+];
+
+console.log(groceries.length);
+
+// Create a Function to Remove Duplicates
+
+var deleteDuplicates = function(list) {
+  var cleanList = [];
+  for ( var item of list ) {
+    if (!cleanList.includes(item)) {
+      cleanList.push(item);
+    }
+  }
+  return cleanList;
+};
+
+// Check the Length of the New Array
+
+var newGroceries = deleteDuplicates(groceries);
+console.log(newGroceries.length);
+
+// Create a Numbered List of the Grocery Items
+
+newGroceries.forEach(function (item, index) {
+  console.log(`#${index + 1} - ${item}. `);
+});
+
